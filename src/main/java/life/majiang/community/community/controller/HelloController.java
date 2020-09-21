97eaef1a40020c8b4f3df5964328c4cd81ab4795
unknown <1520611463@qq.com>
2020-09-21 16:37:34 +0800

    init repo

---
+++ src/main/java/life/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,8 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+    public String hello()
+}
