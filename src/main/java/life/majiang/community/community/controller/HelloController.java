5501b665f105be101a28942c22d378c88bfca726
codedrinker <1520611463@qq.com>
2020-10-02 23:28:24 +0800

    add Index and bootstap

--- src/main/java/life/majiang/community/community/controller/HelloController.java
+++
@@ -1,8 +0,0 @@
-package life.majiang.community.community.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class HelloController {
-    public String hello()
-}
