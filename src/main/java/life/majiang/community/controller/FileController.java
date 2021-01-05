fbd37235bf5c2eaa45eb695e665d4516bc52e135
Omlovee <1520611463@qq.com>
2021-01-05 18:51:01 +0800

    增加markdown编辑

---
+++ src/main/java/life/majiang/community/controller/FileController.java
@@ -0,0 +1,17 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    public FileDTO upload(){
+
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/");
+        return fileDTO;
+    }
+}
